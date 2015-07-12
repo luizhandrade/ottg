3e616506ed2e235e4b04c35d644249435c2d7447
DeAndradeLuiz <luizlhandrade@gmail.com>
2015-07-12 23:08:22 +0200

    Functional test check for input

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -18,19 +19,33 @@ class NewVisitorTest(unittest.TestCase):
 
 # He noticies he is in a to-do list web page by its title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
 # He is invited to add a new item to a list
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do list'
+        )
 # He types "Reserve Paris rugby match ticket" 'cause he loves rugby
+        inputbox.send_keys('Reserve Paris rugby match')
 
 # He hits enter and the page shows
 # "1: Reserver Paris rugby match ticket" as an item in the list
+        inputbox.send_keys(Keys.enter)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Reserver Paris rugby match' for row in rows)
+        )
+
 
 # There is still a text box inviting to write a new item
 # He enters "Reserve hotel in paris"
 
+        self.fail('Finish the test.')
 # The page updates and show both items
 
 # He wonders if still be able to se the list when he closes his browser
